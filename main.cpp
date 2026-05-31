ae34789d0e4523935385f188855b4589c99e9763
pratik0620 <pratikmorkar000@gmail.com>
2026-05-31 14:07:04 +0530

    Refactor code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,70 @@
 #include<unistd.h>
 #include<sys/wait.h>
 
+void buildArgs(std::string tokens[], int token_count, char* args[]) {
+    for(size_t i=0; i<token_count; ++i) {
+        args[i] = new char[tokens[i].length() + 1];
+        std::strcpy(args[i], tokens[i].c_str());
+    }
+    args[token_count] = nullptr;
+}
+
+int tokenize(std::string &user_input, std::string tokens[]) {
+    std::string current_token;
+    int i=0, token_count=0;
+    while(i < user_input.size()) {
+        if(user_input[i] != ' ') {
+            current_token += user_input[i];
+        } else {
+            if(!current_token.empty()) {
+                tokens[token_count++] = current_token;
+                current_token = "";
+            }
+        }
+        i++;
+    }
+    if(!current_token.empty()) {
+        tokens[token_count++] = current_token;
+        current_token = "";
+    }
+    return token_count;
+}
+
+void cleanup(char* args[], int count){
+    for(size_t i=0; i<count; ++i) delete[] args[i];
+}
+
+void executeExternal(char* args[]) {
+    pid_t pid = fork();
+
+    if(pid == 0) {
+        execvp(args[0], args);
+        perror("execvp failed");
+        exit(1);
+    } else if (pid > 0) {
+        wait(NULL);
+    } else {
+        perror("Fork Failed");
+        return;
+    }
+}
+
+bool executeBuiltin(char* args[]) {
+    if(strcmp(args[0], "exit") == 0) {
+        exit(0);
+    } else if (strcmp(args[0], "cd") == 0) {
+        if(args[1] == nullptr) {
+            return true;
+        } else {
+            if(chdir(args[1]) != 0) {
+                std::cout << "Failed to change directory" << std::endl;
+            }
+        }
+        return true; 
+    }
+    return false;
+}
+
 int main() {
     while(true) {
         char buffer[1024];
@@ -14,69 +78,16 @@ int main() {
         std::getline(std::cin, user_input);
         if(user_input.empty()) continue;
 
-        int i=0, j=0;
         std::string tokens[100];
-        std::string current_token;
-        while(i < user_input.size()) {
-            if(user_input[i] != ' ') {
-                current_token += user_input[i];
-            } else {
-                if(!current_token.empty()) {
-                    tokens[j] = current_token;
-                    current_token = "";
-                    j++;
-                }
-            }
-            i++;
-        }
-
-        if(!current_token.empty()) {
-            tokens[j] = current_token;
-            j++;
-            current_token = "";
-        }
-
+        
+        int token_count = tokenize(user_input, tokens);
+        if(token_count == 0) continue;
+        
         char *args[100];
+        buildArgs(tokens, token_count, args);
 
-        if(tokens[0] == "exit") break;
-        else if (tokens[0] == "cd") {
-            for(size_t k=0; k<j; ++k) {
-                args[k] = new char[tokens[k].length() + 1];
-                std::strcpy(args[k], tokens[k].c_str());
-            }
-            args[j] = nullptr;
-
-            if(j < 2) {
-                std::cout << "Usage: cd <directory>\n";
-            } else {
-                if(chdir(args[1]) == 0) {
-                } else {
-                    std::cout << "Failed to change directory" << std::endl;
-                }
-            }    
-        } else {
-            for(size_t k=0; k<j; ++k) {
-                args[k] = new char[tokens[k].length() + 1];
-                std::strcpy(args[k], tokens[k].c_str());
-            }
-            args[j] = nullptr;
-
-            pid_t pid = fork();
-
-            if(pid == 0) {
-                execvp(args[0], args);
-                perror("execvp failed");
-                exit(1);
-            } else if (pid > 0) {
-                wait(NULL);
-            } else {
-                perror("Fork Failed");
-            }
-        }
-
-        for(size_t k=0; k<j; ++k) {
-            delete[] args[k];
-        }
+        if(!executeBuiltin(args)) executeExternal(args);
+        cleanup(args, token_count);
     }
 
     return 0;
